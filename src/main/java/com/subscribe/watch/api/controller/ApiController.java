dcb7f242e11aa80f54e7393ffe63dfb037b027d3
BaeJi77 <hahawjstk@gmail.com>
2019-12-16 19:49:03 +0900

    first commit

---
+++ src/main/java/com/subscribe/watch/api/controller/ApiController.java
@@ -0,0 +1,12 @@
+package com.subscribe.watch.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @GetMapping("/")
+    public String test() {
+        return "hello";
+    }
+}
---
+++ src/test/java/com/subscribe/watch/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subscribe.watch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
