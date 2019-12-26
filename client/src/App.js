68b160ffc4ba171923d85dca8e827b084b596a8b
parkyounghwan <dudrnxps1@gmail.com>
2019-12-26 22:34:10 +0900

    #yh-issue-13, Layout 잡기, 버튼 샘플 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container, Button } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Container className="themed-container">
+          <p>Watch Your Subscribe</p>
+          <Button color="primary" size="lg" block>Google Login button</Button>
+        </Container>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +24,7 @@ function App() {
         >
           Learn React
         </a>
+      </header> */}
       </header>
     </div>
   );
