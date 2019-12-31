dbd336748ff9dec47f582f76ebc651a001172cd3
정성연 <tjdds1109@naver.com>
2019-12-31 15:12:04 +0900

    #sy-issue-2 /login url 입력시 googlelogin redirect

---
+++ src/main/java/com/subscribe/watch/api/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.subscribe.watch.api.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.Filter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final Filter ssoFilter;
+
+  public SecurityConfig(Filter ssoFilter) {
+    this.ssoFilter = ssoFilter;
+    System.out.println(ssoFilter);
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.antMatcher("/**")
+            .authorizeRequests()
+            .antMatchers("/", "/h2-console/**", "/favicon.ico", "/login").permitAll() // "/login**" 옵션 추가
+            .anyRequest().authenticated()
+            .and().logout().logoutSuccessUrl("/").permitAll()
+            .and().headers().frameOptions().sameOrigin()
+            .and().csrf().disable()
+            .addFilterBefore(ssoFilter, BasicAuthenticationFilter.class); // OAuthConfig에서 생성한 ssoFilter 추가
+  }
+}
